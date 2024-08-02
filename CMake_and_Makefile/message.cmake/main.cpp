938d8819d3b81551a7ba8271900e8d29fa374aef
Frederique Hsu <frederique.hsu@outlook.com>
2024-08-03 00:56:57 +0800

    Build and link the static library and shared library in the CMakeLists.txt script.

--- CMake_and_Makefile/message.cmake/main.cpp
+++ CMake_and_Makefile/message.cmake/main.cpp
@@ -1,7 +1,7 @@
 /*!
- *  \file       hello-world.cpp
- *  \brief      
- *  
+ *  \file       main.cpp
+ *  \brief
+ *
  */
 
 
@@ -10,12 +10,16 @@
 #include <cstdlib>
 #include <iostream>
 
+
 int main(int argc, char* argv[])
 {
-    Message say_hello("Hello, CMake world!");
+    (void)argc;
+    (void)argv;
+
+    Message say_hello("Hello, CMake build world!");
     std::cout << say_hello << std::endl;
 
-    Message say_goodbye("Goodbye, CMake world.");
+    Message say_goodbye("Goodbye, CMake build world!");
     std::cout << say_goodbye << std::endl;
 
     return EXIT_SUCCESS;
