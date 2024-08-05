9f9b28ea771ba1c790ea8684ec645d140e8c277a
Frederique Hsu <frederique.hsu@outlook.com>
2024-08-06 02:14:19 +0800

    研究CMake中如何使用if() / endif(),  foreach() / endforeach()等控制流, 以及set_source_file_properties(), get_source_file_properties()

---
+++ CMake_and_Makefile/Control_Flow.cmake/geometry_rhombus.cpp
@@ -0,0 +1,20 @@
+/*!
+ *  \file       geometry_rhombus.cpp
+ *  \brief
+ *
+ */
+
+
+#include "geometry_rhombus.hpp"
+
+
+namespace geometry
+{
+namespace area
+{
+double rhombus(double d1, double d2)
+{
+    return (d1 * d2) / 2.0;
+}
+}
+}
