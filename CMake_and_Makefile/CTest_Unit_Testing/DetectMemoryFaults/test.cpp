fb541d126968d8c1a95ebb404f6bbbaf2ed3b939
Frederique Hsu <frederique.hsu@outlook.com>
2024-08-13 15:52:03 +0800

    研究CMake中加入valgrind检测内存泄漏

---
+++ CMake_and_Makefile/CTest_Unit_Testing/DetectMemoryFaults/test.cpp
@@ -0,0 +1,16 @@
+/*!
+ *  \file       test.cpp
+ *  \brief
+ *
+ */
+
+
+#include "leaky_implementation.hpp"
+
+
+int main(int argc, char* argv[])
+{
+    int code = do_some_work();
+
+    return code;
+}
