39bbad77e7072ed034b47a24fca253b0af7a530d
Frederique Hsu <frederique.hsu@outlook.com>
2024-09-26 01:58:25 +0800

    从GitHub clone下来整个Boost库,并从源码编译得到静态库libboost_unit_test_framework.a
    Hierarchies 和 Interface_Polymorphism 两个子项目使用了该静态库, 所以改掉原来的动态链接, 相应地user main()函数不需要了,改为使用静态库提供的main()函数.

--- abstraction_mechanisms/Hierarchies/main.cpp
+++ abstraction_mechanisms/Hierarchies/main.cpp
@@ -6,21 +6,27 @@
 
 
 #define BOOST_TEST_MODULE "unit test for Hierarchies project."
-#define BOOST_TEST_NO_MAIN
+// #define BOOST_TEST_DYN_LINK
+// #define BOOST_TEST_NO_MAIN
 #include <boost/test/unit_test.hpp>
 
-#include <iostream>
+#if false
+    int main(int argc, char* argv[], char* envp[])
+    {
+        std::cout << "Current C++ standard no. is " << __cplusplus << std::endl;
+        std::cout << "Current program is " << sizeof(void*) * 8 << "-bits.\n" << std::endl;
 
-
-int main(int argc, char* argv[], char* envp[])
-{
-    std::cout << "Current C++ standard no. is " << __cplusplus << std::endl;
-    std::cout << "Current program is " << sizeof(void*) * 8 << "-bits.\n" << std::endl;
-
-    return boost::unit_test::unit_test_main(&init_unit_test, argc, argv);
-}
+        return boost::unit_test::unit_test_main(&init_unit_test, argc, argv);
+    }
+#endif
 
 BOOST_AUTO_TEST_CASE(DummyUnitTest)
 {
+    std::cout << "Current C++ standard no. is " << __cplusplus << std::endl;
+    std::cout << "Current program is " << sizeof(void*) * 8 << "-bits.\n" << std::endl;
+    
+    int* array = new int[10];   // Just for valgrind to examine the memory leakage purpose.
+    delete [] array;
+    
     BOOST_CHECK(true);
 }
--- abstraction_mechanisms/Interface_Polymorph/main.cpp
+++ abstraction_mechanisms/Interface_Polymorph/main.cpp
@@ -6,20 +6,22 @@
 
 
 #define BOOST_TEST_MODULE "Unit testing for Interface_Polymorph project"
-#define BOOST_TEST_NO_MAIN
+// #define BOOST_TEST_DYN_LINK
+// #define BOOST_TEST_NO_MAIN
 #include <boost/test/unit_test.hpp>
 
-#include <iostream>
-
-
-int main(int argc, char* argv[])
-{
-    std::cout << "Current C++ standard no. is " << __cplusplus << std::endl;
-    
-    return boost::unit_test::unit_test_main(&init_unit_test, argc, argv);
-}
+#if false
+    int main(int argc, char* argv[])
+    {
+        std::cout << "Current C++ standard no. is " << __cplusplus << std::endl;
+        
+        return boost::unit_test::unit_test_main(&boost_init_unit_test, argc, argv);
+    }
+#endif
 
 BOOST_AUTO_TEST_CASE(DummyUnitTest)
 {
+    std::cout << "Current C++ standard no. is " << __cplusplus << std::endl;
+    
     BOOST_CHECK(true);
 }
